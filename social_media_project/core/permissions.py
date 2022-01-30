2fcc5a14c337ec26f6dc5aa7212c214fc8693e59
Kevin <edilson4football@gmail.com>
2022-01-30 13:57:04 +0200

    Fix: Solve permission bug where only the author of the post had access to details

--- social_media_project/core/permissions.py
+++ social_media_project/core/permissions.py
@@ -7,5 +7,9 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
         print(obj.author, request.user)
         return obj.author == request.user
