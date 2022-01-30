80a7630cd7662882ea8e071ff6a898314ce1f9cb
Kevin <edilson4football@gmail.com>
2022-01-30 21:07:36 +0200

    Fix : Post Author on creation is request.user

--- social_media_project/core/models.py
+++ social_media_project/core/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name="likes")
+    likes = models.ManyToManyField(User, blank=True, related_name="likes")
 
     def get_num_likes(self):
         return self.likes.count()
--- social_media_project/core/serializers.py
+++ social_media_project/core/serializers.py
@@ -3,6 +3,9 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Post
         fields = '__all__'
+        read_only_fields = ['author']
